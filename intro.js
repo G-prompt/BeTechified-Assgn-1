e32d9f01fc56c9152643ad0757aee0eebdc7dda1
Goodluck <githubprompt@gmail.com>
2026-01-12 17:04:08 +0100

    initial commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+let firstname = "Oreoluwa";
+let email = "goodluckelisha21@gmail.com";
+let favourite_programming_language = "JavaScript";
+
+console.log("hello", firstname)
+console.log("email", email)
+console.log("favourite programming language", favourite_programming_language)
